3b78ee614f5642dba76748afa98947070022fe6e
saiyednr <117348555+saiyednr@users.noreply.github.com>
2025-03-27 15:06:25 +0530

    Update settings.py

--- abgfm/settings.py
+++ abgfm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8pckuqhdir_yyw#4#827tqtxb=ru-1whdyq@9qpooln3$8fs!(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
